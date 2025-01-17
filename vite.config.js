42ee27ba2cc9010c2a1eb57f900673fb8582fa0f
Rohan <rohan.garad@example.com>
2025-01-17 21:18:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/socket.io": {
-        target: "http://localhost:5000",
+        target: "https://rohan-garad-full-stack-backend.onrender.com",
         ws: true,
       },
     },
